0b25b13a1d31af2eb0621cf4aa7687cf76eace4c
gunhasiz <koflus@hotmail.com>
2026-04-10 18:03:04 +0200

    Minor changes

--- src/recon/cli/commands.py
+++ src/recon/cli/commands.py
@@ -5,7 +5,7 @@ app = typer.Typer(help="Kresy-Siberia 2KPL Recon Tool")
 @app.command()
 def start(pages: int = 5):
     """Run the recon download process."""
-    typer.echo(f"Rozpoczynam pobieranie danych.")
+    typer.echo(f"Starting data collection.")
 
 if __name__ == "__main__":
     app()
