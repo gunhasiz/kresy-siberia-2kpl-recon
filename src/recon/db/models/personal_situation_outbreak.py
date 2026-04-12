ac4ccbcb691d8e9fed4af9a26fd99b9df1ee561a
gunhasiz <koflus@hotmail.com>
2026-04-12 23:24:41 +0200

    Add Personal Situation at the outbreak of WWll DB model

---
+++ src/recon/db/models/personal_situation_outbreak.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.relationships import _RelationshipDeclared
+from .base import Base
+
+class PersonalSituationOutbreak1939(Base):
+    __tablename__: str = "personal_situation_outbreaks_1939"
+
+    id: Column[int] = Column(Integer, primary_key=True)
+    person_id: Column[int] = Column(Integer, ForeignKey("people.id"), nullable=False)
+    
+    # Residence at the outbreak of WWII
+    residence: Column[str] = Column(String, nullable=True)
+    # Kresy Inhabitant Status:
+    kresy_inhabitant_status: Column[str] = Column(String, nullable=True)
+    # Ethnicity
+    ethnicity: Column[str] = Column(String, nullable=True)
+    # Religion
+    religion: Column[str] = Column(String, nullable=True)
+    # Education Level
+    education_level: Column[str] = Column(String, nullable=True)
+    # Occupation at the outbreak of WWII
+    occupation: Column[str] = Column(String, nullable=True)
+    # Military status at the outbreak of WWII
+    military_status: Column[str] = Column(String, nullable=True)
+    # Military Rank at the outbreak of WWII
+    military_rank: Column[str] = Column(String, nullable=True)
+
+    person: _RelationshipDeclared[Any] = relationship("Person", back_populates="initial_status")
