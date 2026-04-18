30fcf76989eaf73b8ec701762434d18e0c081264
gunhasiz <koflus@hotmail.com>
2026-04-18 16:04:33 +0200

    Add OccupationPerdio DB model

---
+++ src/recon/db/models/occupation_period.py
@@ -0,0 +1,24 @@
+from datetime import date
+from typing import Any
+
+from sqlalchemy import Column, Integer, String, Date, Text, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.relationships import _RelationshipDeclared
+from .base import Base
+
+class OccupationPeriod(Base):
+    __tablename__: str = "occupation_periods"
+
+    id: Column[int] = Column(Integer, primary_key=True)
+    person_id: Column[int] = Column(Integer, ForeignKey("persons.id"), nullable=False)
+
+    # Place of province residence during occupation period
+    province: Column[str] = Column(String, nullable=True)
+    # Place of district residence during occupation period
+    county: Column[str] = Column(String, nullable=True)
+    # Place of city residence during occupation period
+    city_place: Column[str] = Column(String, nullable=True)
+    # Nearest large city to place of residence during occupation period
+    nearest_large_city: Column[str] = Column(String, nullable=True)
+
+    person: _RelationshipDeclared[Any] = relationship("Person", back_populates="occupation_periods")
