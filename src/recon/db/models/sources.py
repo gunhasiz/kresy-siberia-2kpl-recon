eb547329555b9fe07ba210fb6c9a3589a3a72133
gunhasiz <koflus@hotmail.com>
2026-04-18 18:33:43 +0200

    Add Sources DB model

---
+++ src/recon/db/models/sources.py
@@ -0,0 +1,24 @@
+from typing import Any
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm.relationships import _RelationshipDeclared
+from .base import Base
+
+
+class Sources(Base):
+    __tablename__: str = "sources"
+    
+    id: Column[int] = Column(Integer, primary_key=True)
+    person_id: Column[int] = Column(
+        Integer, ForeignKey("persons.id"), nullable=False)
+
+    # A brief summary or description of the source
+    summary = Column(String, nullable=True)
+    # URL or reference to the source in string form
+    url_text = Column(String, nullable=True)
+    # URL or reference to the source
+    url = Column(String, nullable=True)
+
+    person: _RelationshipDeclared[Any] = relationship(
+        "Person", back_populates="sources")
