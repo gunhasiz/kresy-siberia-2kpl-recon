bda2fb1c840f01ab76b34cd79d080d29d482b622
gunhasiz <koflus@hotmail.com>
2026-04-18 23:49:21 +0200

    Add base config

---
+++ src/recon/core/config.py
@@ -0,0 +1,18 @@
+from os import getenv
+from typing import Optional
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Config:
+    DATABASE_URL: Optional[str] = getenv("DATABASE_URL")
+    DEBUG: bool = getenv("DEBUG", "False").lower() == "true"
+    
+    BASE_URL: Optional[str] = getenv("BASE_URL")
+    USER_AGENT: Optional[str] = getenv("USER_AGENT")
+    DELAY_MIN: float = float(getenv("DELAY_MIN", 1.0))
+    DELAY_MAX: float = float(getenv("DELAY_MAX", 3.0))
+
+config = Config()
