327818bb27fac512a95b05499d86977941d186ff
gunhasiz <koflus@hotmail.com>
2026-04-20 16:39:39 +0200

    Add constants

---
+++ src/recon/helpers/constants.py
@@ -0,0 +1,13 @@
+# constants.py
+# This file contains constants used across the recon project.
+
+# H4 section titles on the record page
+PERSONAL_DETAILS: str = r"Personal Details"
+PERSONAL_SITUATION_OUTBREAK: str = r"Personal Situation at the outbreak of WWll"
+DEPORTATIONS_AND_REPRESSIONS: str = r"Deportations and Repressions"
+REPATRIATION: str = r"For those who were repatriated to Poland from the Kresy or the USSR, please provide the following information"
+OCCUPATION_PERIOD: str = r"For those who stayed in the Kresy area during WWII, please provide the following information"
+MILITARY_EXPERIENCE: str = r"Military Experience"
+OTHER_MILITARY_EXPERIENCE: str = r"Other Wartime Circumstances"
+SOURCES: str = r"Sources"
+RELATED_GALLERIES: str = r"Related Museum Galleries"
